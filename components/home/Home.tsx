c095f1cd85be5a4db649c3d09e7745bd420d710c
usmailaabdoul <ismaelabdul77@gmail.com>
2024-02-04 18:33:30 +0100

    fix

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -34,7 +34,7 @@ export const Home = () => {
             }}
           />
           <div className="relative flex flex-1 flex-col space-y-8">
-            <div className="absolute flex flex-col md:flex-row justify-center pl-48 md:pl-0 md:-mt-[400px] w-full">
+            <div className="absolute flex flex-col md:flex-row justify-center pl-48 -mt-[400px] w-full">
               <div className="bg-[#1C9FE9] w-56 h-56 md:w-96 md:h-96 rounded-full blur-[70px] md:blur-[90px] mr-32 opacity-60"></div>
               <div className="bg-[#7000FF] w-56 h-56 md:w-96 md:h-96 rounded-full blur-[70px] md:blur-[90px] mt-12 opacity-60"></div>
             </div>
