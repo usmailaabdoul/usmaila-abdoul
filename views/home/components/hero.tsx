2c5287f60d0185dd70045331ae37d7affea010e0
usmaila abdoul <ismaelabdul77@gmail.com>
2025-03-15 15:21:01 +0100

    updating hero section

--- views/home/components/hero.tsx
+++ views/home/components/hero.tsx
@@ -18,12 +18,12 @@ export default function Hero() {
           <Socials />
         </Box>
         <Box className='border-0 grid grid-cols-7'>
-          {new Array(28).fill('').map((val, id) => {
+          {new Array(35).fill('').map((val, id) => {
             const row = Math.floor(id / 7);
             const col = id % 7;
 
             const rightEdge = col === 6;
-            const bottomEdge = row === 3;
+            const bottomEdge = row === 4;
 
             return (
               <Box key={id} className={cn(
@@ -37,17 +37,6 @@ export default function Hero() {
           })}
 
           <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 w-[calc(100%_-_100px)] flex flex-col items-center justify-center p-4">
-            <div className="absolute inset-0 flex items-center justify-center">
-              <div className="w-full h-4/5 bg-transparent backdrop-blur-[2px]" 
-                style={{
-                  background: 'radial-gradient(circle at center, rgba(0,0,0,0.7) 0%, transparent 70%)'
-                }} />
-            </div>
-
-            <div className="absolute -inset-0 -z-10">
-              <div className="absolute inset-0 bg-gradient-to-br from-blue-500/20 via-emerald-500/20 to-orange-500/20 opacity-90" />
-            </div>
-
             <div className="relative w-44 h-44 z-10">
               <div className="absolute inset-0 backdrop-blur-sm overflow-hidden">
                 <Image 
@@ -68,12 +57,19 @@ export default function Hero() {
           const row = Math.floor(id / 10);
           const col = id % 10;
 
+          const emptyRows = row !== 0 && row !== 7 && col !== 0 && col !== 9;
+
           const rightEdge = col === 9;
           const bottomEdge = row === 7;
+          const emptyRightEdge = col == 8;
+          const emptyBottomEdge = row == 6;
 
           return (
             <Box key={id} className={cn(
               'min-h-24 border-t-0 border-l-0',
+              emptyRows && 'border-0',
+              emptyRows && emptyRightEdge && 'border-r-[1.5px]',
+              emptyRows && emptyBottomEdge && 'border-b-[1.5px]',
               bottomEdge && 'border-b-0',
               rightEdge && 'border-r-0',
             )}>
@@ -81,28 +77,17 @@ export default function Hero() {
             </Box>
           )
         })}
-        <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 w-[calc(100%_-_230px)] flex flex-col items-center justify-center p-14">
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div className="w-full max-w-3xl h-4/5 bg-transparent backdrop-blur-[2px]" 
-              style={{
-                background: 'radial-gradient(circle at center, rgba(0,0,0,0.7) 0%, transparent 70%)'
-              }} />
-          </div>
-
-          <h1 className="text-5xl font-bold mb-8 relative z-10 max-w-3xl text-center">
+        <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 w-[calc(100%_-_230px)] flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-bold mb-5 relative z-10 max-w-3xl text-center">
             Hi, I&apos;m Usmaila Abdoul
           </h1>
           
-          <p className="dark:text-gray-300 text-xl mb-12 max-w-2xl mx-auto text-center relative z-10 px-8">
+          <p className="dark:text-gray-300 text-xl text-center px-5 mb-8">
             A Software Engineer<br/>
             With more than 4 years of experience, i am passionate about building web and mobile applications. I love the challenge of taking an idea and turning it into a working product that people can use.
           </p>
           
-          <div className="absolute -inset-0 -z-10">
-            <div className="absolute inset-0 bg-gradient-to-br from-blue-500/20 via-emerald-500/20 to-orange-500/20 opacity-90" />
-          </div>
-          
-          <div className="relative w-64 h-64 z-10">
+          <div className="relative w-60 h-60">
             <div className="absolute inset-0 backdrop-blur-sm overflow-hidden">
               <Image 
                 src="/usmaila.png" 
@@ -114,8 +99,7 @@ export default function Hero() {
               />
             </div>
           </div>
-
-          <Socials />
+          <Socials className='mt-5'/>
         </div>
       </Box>
     </>
